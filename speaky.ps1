8e8540fd619f69c9d3e208bd911e2f4f351d9b18
notnedo <notnedeo@protonmail.com>
2020-07-01 19:32:06 -0700

    Initial commit

---
+++ speaky.ps1
@@ -0,0 +1,36 @@
+﻿Add-Type -assembly System.Windows.Forms
+Add-Type -AssemblyName System.speech
+
+$Narrator = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$Narrator.SelectVoice('Microsoft David Desktop')
+$Narrator.Rate = 0
+
+$mainForm = New-Object System.Windows.Forms.Form
+$mainForm.Text ='Text to Speech'
+$mainForm.Width = 500
+$mainForm.Height = 500
+$mainForm.AutoSize = $true
+
+
+$InputButton = New-Object System.Windows.Forms.Button
+$InputButton.Text = 'Speak'
+$InputButton.Location = '200,200'
+
+
+
+$InputBox = New-Object System.Windows.Forms.TextBox
+$InputBox.Location = New-Object System.Drawing.Size(170,50)
+$InputBox.Size = New-Object System.Drawing.Size(150,20)
+
+$mainForm.Controls.Add($InputBox) 
+$mainForm.Controls.Add($InputButton)
+
+
+$InputButton.Add_Click({
+
+$Speech = $InputBox.Text
+$Narrator.Speak($Speech)
+
+})
+
+$mainForm.ShowDialog()
